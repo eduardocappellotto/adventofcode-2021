545b82c2873d5886217936856e2fa87f77c4157f
eduardocappellotto <eduardocappellotto@gmail.com>
2022-04-27 00:09:59 -0300

    challenge 1 - parte 2

--- challenge-1/index.js
+++ challenge-1/index.js
@@ -19,30 +19,31 @@ async function partOne() {
     }
   }
 
-  console.log(increased);
+  console.log("Aumentos:", increased);
 }
-partOne();
 
 async function partTwo() {
   let dephts;
   try {
     dephts = fs.readFileSync("./input.txt", "utf8").split("\r\n").map(Number);
-    console.log(dephts);
   } catch (err) {
     console.error(err);
   }
   let increased = 0;
 
-  for (let index = 1; index < dephts.length; index++) {
-    if (dephts[index] > dephts[index - 1]) {
-      console.log(dephts[index], dephts[index - 1], "increased");
+  let sums = dephts.map((depth, index) => {
+    if (dephts[index + 1] && dephts[index + 2]) {
+      return dephts[index] + dephts[index + 1] + dephts[index + 2];
+    }
+  });
+
+  for (let index = 1; index < sums.length - 2; index++) {
+    if (sums[index] > sums[index - 1]) {
       increased++;
-    } else {
-      console.log(dephts[index], dephts[index - 1]);
     }
   }
 
-  console.log(increased);
+  console.log("Aumentos:", increased);
 }
 // partOne();
 partTwo();
